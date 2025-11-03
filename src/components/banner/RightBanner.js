785b5fe28aa687bd27abf3a4face96609dcf4ada
Paudel <atharvapaudel4@gmail.com>
2025-11-03 10:28:54 +0545

    removed image (right banner)

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -7,11 +7,11 @@ import { bannerImg } from "../../assets/index";
 const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
-      <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+      {/* <img
+        className="w-[200px] h-[200px] lgl:w-[500px] lgl:h-[380px] z-10 rounded-lg"
         src={bannerImg}
         alt="bannerImg"
-      />
+      /> */}
       <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
     </div>
   );
